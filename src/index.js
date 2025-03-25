a45b6aeead19ac4b88755e9cce0587c3ee794cfb
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2025-03-25 14:21:28 +0700

    first time commit pj file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Login from './components/loginComponent/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>                          
+    </BrowserRouter>
+  
   </React.StrictMode>
 );
 
---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../src/components/dashBComponent/dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <h1 className='title'>To-do List Dashboard</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
